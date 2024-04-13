eed11c0b1f4869e91a149090bd48b49308f0ae0c
ChenCoin <chencoin.cn@gmail.com>
2024-04-13 15:50:39 +0800

    make blackboard look good

--- lib/view/black_board.dart
+++ lib/view/black_board.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../data/GridData.dart';
+
 class BlackBoard extends StatelessWidget {
   final Size size;
 
@@ -18,12 +20,62 @@ class _BlackBoard extends CustomPainter {
     var rect = Offset.zero & size;
     var paint = Paint()
       ..isAntiAlias = true
-      ..style = PaintingStyle.fill //填充
-      ..color = const Color(0xff1e7f56);
-    canvas.drawRRect(RRect.fromRectAndRadius(rect, const Radius.circular(8)), paint);
+      ..style = PaintingStyle.fill
+      ..color = const Color(0xFFC1802E);
+    // 画棕黄色的底
+    canvas.drawRRect(
+        RRect.fromRectAndRadius(rect, const Radius.circular(8)), paint);
+
+    // 画墨绿色的底
+    paint.color = const Color(0xFF1E7F56);
+    Rect innerBox = rectMap(rect, 10);
+    canvas.drawRRect(
+        RRect.fromRectAndRadius(innerBox, const Radius.circular(8)), paint);
+
+    // 画棋盘线
+    drawBackground(
+        canvas, rectMap(rect, 10), (rect.width - 20) / (GridData.col));
+
+    // 画黄褐色的最外圈边框
+    paint.style = PaintingStyle.stroke;
+    paint.color = const Color(0xFF4E3111);
+    paint.strokeWidth = 2;
+    canvas.drawRRect(
+        RRect.fromRectAndRadius(rect, const Radius.circular(8)), paint);
+
+    // 画深绿色的内圈边框
+    paint.color = const Color(0xFF0A412A);
+    paint.strokeWidth = 3;
+    Rect innerRect = rectMap(rect, 9);
+    canvas.drawRRect(
+        RRect.fromRectAndRadius(innerRect, const Radius.circular(8)), paint);
   }
 
-  // 返回false, 后面介绍
   @override
   bool shouldRepaint(CustomPainter oldDelegate) => false;
+
+  void drawBackground(Canvas canvas, Rect rect, double grid) {
+    var paint = Paint()
+      ..isAntiAlias = true
+      ..style = PaintingStyle.stroke
+      ..color = const Color(0xFF56B293)
+      ..strokeWidth = 1.0;
+
+    // 画横线
+    for (int i = 1; i < GridData.row; i++) {
+      double dy = rect.top + grid * i;
+      canvas.drawLine(Offset(rect.left, dy), Offset(rect.right, dy), paint);
+    }
+
+    // 画竖线
+    for (int i = 1; i < GridData.col; i++) {
+      double dx = rect.left + grid * i;
+      canvas.drawLine(Offset(dx, rect.top), Offset(dx, rect.bottom), paint);
+    }
+  }
+
+  Rect rectMap(Rect src, int diff) {
+    return Rect.fromLTRB(
+        src.left + diff, src.top + diff, src.right - diff, src.bottom - diff);
+  }
 }
--- lib/view/num_board.dart
+++ lib/view/num_board.dart
@@ -14,39 +14,13 @@ class NumBoard extends StatefulWidget {
 }
 
 class _NumBoardState extends State<NumBoard> {
-  int score = 0;
-
   @override
   Widget build(BuildContext context) {
-    print('number build');
-    bool repaint = score == widget.data.score;
-    score = widget.data.score;
     var size = widget.size;
     return CustomPaint(
       size: size,
-      painter:
-          MyPainter(width: size.width, data: widget.data, repaint: repaint),
+      painter: MyPainter(width: size.width, data: widget.data),
     );
-  }
-}
-
-void drawBackground(Canvas canvas, Rect rect, double grid) {
-  //画棋盘网格
-  var paint = Paint()
-    ..isAntiAlias = true
-    ..style = PaintingStyle.stroke //线
-    ..color = const Color(0xFF56B293)
-    ..strokeWidth = 1.0;
-
-  //画横线
-  for (int i = 0; i <= GridData.row; i++) {
-    double dy = rect.top + grid * i;
-    canvas.drawLine(Offset(rect.left, dy), Offset(rect.right, dy), paint);
-  }
-
-  for (int i = 0; i <= GridData.col; i++) {
-    double dx = rect.left + grid * i;
-    canvas.drawLine(Offset(dx, rect.top), Offset(dx, rect.bottom), paint);
   }
 }
 
@@ -94,24 +68,17 @@ class MyPainter extends CustomPainter {
 
   double gridHeight = 0;
 
-  final bool repaint;
-
-  MyPainter({required this.width, required this.data, required this.repaint}) {
+  MyPainter({required this.width, required this.data}) {
     grid = width / (GridData.col);
     gridHeight = grid * (GridData.row);
   }
 
   @override
   void paint(Canvas canvas, Size size) {
-    print('draw background and number');
     var rect = Offset.zero & Size(size.width, gridHeight);
-    //画棋盘
-    drawBackground(canvas, rect, grid);
-    //画棋子
     drawPieces(canvas, rect, grid, data);
   }
 
-  // 返回false, 后面介绍
   @override
   bool shouldRepaint(CustomPainter oldDelegate) => true;
 }
