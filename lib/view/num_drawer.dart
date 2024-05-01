2d7f2bdb926a03962e3f6d137d4cbac6bd371567
ChenCoin <chencoin.cn@gmail.com>
2024-05-01 10:44:57 +0800

    num 10 color is yellow

--- lib/view/num_drawer.dart
+++ lib/view/num_drawer.dart
@@ -6,6 +6,11 @@ class NumberDrawer {
     ..style = PaintingStyle.fill
     ..color = Colors.white;
 
+  final yellowGridPaint = Paint()
+    ..isAntiAlias = true
+    ..style = PaintingStyle.fill
+    ..color = Colors.amber;
+
   final paint = TextPainter()
     ..textAlign = TextAlign.center
     ..textDirection = TextDirection.ltr;
@@ -18,11 +23,18 @@ class NumberDrawer {
 
   var style = const TextStyle();
 
+  var whiteStyle = const TextStyle();
+
   NumberDrawer(this.grid) {
     p = (grid / 6).floor();
     fontSize = grid - p * 2 - 5;
     style = TextStyle(
       color: Colors.black,
+      fontSize: fontSize,
+      fontWeight: FontWeight.bold,
+    );
+    whiteStyle = TextStyle(
+      color: Colors.white,
       fontSize: fontSize,
       fontWeight: FontWeight.bold,
     );
@@ -46,24 +58,27 @@ class NumberDrawer {
   }
 
   void drawDirect(Canvas canvas, double x, double y, int content) {
-    _drawRaw(canvas, x, y, content, p);
+    _draw(canvas, x, y, content, false);
   }
 
   void drawDirectBold(Canvas canvas, double x, double y, int content) {
-    _drawRaw(canvas, x, y, content, (p * 2 / 3).ceil());
+    _draw(canvas, x, y, content, true);
   }
 
-  void _drawRaw(Canvas canvas, double x, double y, int content, int border) {
+  void _draw(Canvas canvas, double x, double y, int num, bool bold) {
     // 画小白块
+    var border = bold ? (p / 2).ceil() : p;
     var left = grid * x + border;
     var top = grid * y + border;
     var right = left + grid - border * 2;
     var bottom = top + grid - border * 2;
     var rect = Rect.fromLTRB(left, top, right, bottom);
-    RRect whiteGrid = RRect.fromRectAndRadius(rect, const Radius.circular(2));
-    canvas.drawRRect(whiteGrid, gridPaint);
+    Radius radius = Radius.circular(bold ? 6 : 4);
+    RRect whiteGrid = RRect.fromRectAndRadius(rect, radius);
+    canvas.drawRRect(whiteGrid, bold ? yellowGridPaint : gridPaint);
     // 画数字
-    paint.text = TextSpan(text: content.toString(), style: style);
+    paint.text =
+        TextSpan(text: num.toString(), style: bold ? whiteStyle : style);
     paint.layout(minWidth: grid);
     // -2是将文字上移一点
     var dy = grid * y + (grid - fontSize) / 2 - 2;
